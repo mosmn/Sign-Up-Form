634f8ace95c59fa4ae07bc70260d4d9ffb201851
mosmn <ithinkisee8@gmail.com>
2023-01-14 12:37:28 +0800

    updated the js function validatePassword

--- main.js
+++ main.js
@@ -1,41 +1,32 @@
 /*
-BEGIN
-    select password input
-    select confirm password input
-    select error message
+select element id password
+select element id confirm_password
 
-    check if confirm password input is empty
-    if it is not empty 
-        check if password input value is equal to confirm password input value
-        if it is equal
-            set error message to '*Passwords match'
-            set error message color to green
-        if it is not equal
-            set error message to '*Passwords do not match'
-            set error message color to red
-END
-*/
+if password length doesnot = 0
+compare the value of the value attribute of the two elements
+if they are the same, its valid and :valid will be styles will be applied
+if they are not the same, its invalid and :invalid will be styles will be applied
 
-const passwordInput = document.querySelector('#password');
-const confirmPasswordInput = document.querySelector('#confirm-password');
-const errorMessage = document.querySelector('#error-message');
+to create new element
+*/ 
 
-confirmPasswordInput.addEventListener('input', () => {
-    if (confirmPasswordInput.value !== '') {
-        if (passwordInput.value === confirmPasswordInput.value) {
-            errorMessage.textContent = '*Passwords match';
-            errorMessage.style.color = 'green';
-        } else {
-            errorMessage.textContent = '*Passwords do not match';
-            errorMessage.style.color = 'red';
-        }
+const password = document.getElementById('password');
+const confirm_password = document.getElementById('confirm-password');
+const errorMessages = document.getElementById('error-message');
+
+function validatePassword(){
+    if(password.value != confirm_password.value) {
+        confirm_password.setCustomValidity("*Passwords Don't Match");
+        errorMessages.innerHTML = "Passwords Don't Match";
+        errorMessages.style.color = "red";
     } else {
-        errorMessage.textContent = '';
+        confirm_password.setCustomValidity('');
+        errorMessages.innerHTML = "Passwords Match!";
+        errorMessages.style.color = "green";
     }
-});
+}
 
-
-
-
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
 
 
